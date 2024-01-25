540df99709a8a51e64974d43fdd3187ad7af4539
LucyRTCodes <lucyrtutton@gmail.com>
2024-01-25 14:46:56 +0000

    style reveal game

--- pages/reveal-game.js
+++ pages/reveal-game.js
@@ -6,27 +6,28 @@ function RevealGame() {
   const [turn, setTurn] = useState(0);
   return (
     <>
-      <h1>Reveal Game</h1>
-      <h2>Score: {score}</h2>
-      <h2>Turn: {turn}</h2>
-      {RevealGameAnswers.map((_, i) => {
-        return (
-          <RevealImage
-            key={i}
-            position={i}
-            setScore={setScore}
-            turn={turn}
-            setTurn={setTurn}
-          />
-        );
-      })}
-      {/* <RevealImage
+      <div>
+        <h1>Reveal Game</h1>
+        <h2>Score: {score}</h2>
+        {RevealGameAnswers.map((_, i) => {
+          return (
+            <RevealImage
+              key={i}
+              position={i}
+              setScore={setScore}
+              turn={turn}
+              setTurn={setTurn}
+            />
+          );
+        })}
+        {/* <RevealImage
         key={0}
         position={0}
         setScore={setScore}
         turn={turn}
         setTurn={setTurn}
       /> */}
+      </div>
     </>
   );
 }
